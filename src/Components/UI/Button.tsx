a114445793fc5868b1004cb3cf97943f5ca0adff
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-24 00:04:22 -0300

    Ajust className

--- src/Components/UI/Button.tsx
+++ src/Components/UI/Button.tsx
@@ -12,7 +12,7 @@ export function Button({
   disabled?: boolean
 }) {
   let cssName =
-    'bg-slate-800 text-slate-300 py-2 px-5 rounded-md hover:bg-slate-300 hover:text-slate-800 border-2 border-slate-800 '
+    'bg-slate-800 text-slate-300 py-2 px-5 rounded-md hover:bg-slate-300 hover:text-slate-800 border-2 border-slate-800 transition duration-600 ease-in-out '
   if (textOnly) {
     cssName =
       'text-slate-800 bg-slate-300 py-2 px-5 rounded-md hover:text-slate-300 hover:bg-slate-800 border-2 border-slate-800 '
