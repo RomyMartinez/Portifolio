e8d6f6e2d8d9413f64b71995178c51117b0315e4
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-24 00:02:59 -0300

    Add githubLinkendin, delete Button

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Title } from './UI/Title'
-import { Button } from './UI/Button'
+import { GithubLinkendin } from './UI/GithubLinkedin'
 export function Contact() {
   const [isCopy, setIsCopy] = useState(false)
 
@@ -11,24 +11,30 @@ export function Contact() {
 
     setTimeout(() => {
       setIsCopy(false)
-    }, 5000)
+    }, 1000)
   }
 
   return (
-    <section className="flex flex-col w-full justify-center h-svh items-center">
+    <section
+      id="contact"
+      className="flex flex-col w-full justify-center h-svh items-center"
+    >
       <Title title="Contact" extraClass=" text-center pb-5" />
-      <div className="bg-slate-800 rounded-xl h-80 w-96">
-        <div className="flex flex-col text-slate-300 gap-2 items-center content-center">
-          <p>ngcsmrtnz@gmail.com</p>
-          <Button
+      <div className="flex flex-col bg-slate-100 border-2 border-slate-800 rounded-xl justify-center items-center p-5 h-80 w-96 gap-2">
+        <div>
+          <p className="border-2 border-slate-800 rounded-t-md w-44 py-1 text-center">
+            ngcsmrtnz@gmail.com
+          </p>
+          <button
             onClick={handleCopy}
-            textOnly
+            className=" bg-slate-800 text-slate-300 py-1 w-44 rounded-b-md hover:bg-slate-300 hover:text-slate-800 border-2 border-slate-800 transition duration-600 ease-in-out "
             disabled={isCopy ? true : false}
           >
-            Copy email
-          </Button>
-          <p>Phone: +5522999371943</p>
+            {isCopy ? 'Email copied!' : 'Copy email'}
+          </button>
         </div>
+        <p>Phone: +55 22 999371943</p>
+        <GithubLinkendin />
       </div>
     </section>
   )
