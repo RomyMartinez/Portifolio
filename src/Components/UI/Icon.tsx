0ed83442d6d680b724bb0522dc355f35ef8bd943
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-23 23:14:44 -0300

    nothing

--- src/Components/UI/Icon.tsx
+++ src/Components/UI/Icon.tsx
@@ -3,6 +3,7 @@ import React from '../../assets/React.svg'
 export function Icon({ name }: { name: string }) {
   let icon
   if (name === 'React') icon = React
+
   return (
     <>
       <img src={icon} className="h-8" alt={name} />
