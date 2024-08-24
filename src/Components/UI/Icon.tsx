8a402f226a00c5527dc3a6547a523f2d2449971c
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-24 00:04:42 -0300

    Change size icon

--- src/Components/UI/Icon.tsx
+++ src/Components/UI/Icon.tsx
@@ -1,12 +1,14 @@
-import React from '../../assets/React.svg'
+import React from '../../assets/react.png'
+import Css from '../../assets/css.png'
 
 export function Icon({ name }: { name: string }) {
   let icon
   if (name === 'React') icon = React
+  if (name === 'css') icon = Css
 
   return (
     <>
-      <img src={icon} className="h-8" alt={name} />
+      <img src={icon} className="h-7" alt={name} />
     </>
   )
 }
