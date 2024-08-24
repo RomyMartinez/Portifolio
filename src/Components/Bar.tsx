832af99eca7329a6e1a0568a98d67f5f9020fc64
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-23 22:43:59 -0300

    Bar component

---
+++ src/Components/Bar.tsx
@@ -0,0 +1,27 @@
+interface LinkToSectionProps {
+  section: string
+  children: React.ReactNode
+}
+
+function LinkToSection({ section, children }: LinkToSectionProps) {
+  return (
+    <a href={`${section}`} className="no-underline hover:text-slate-500">
+      {children}
+    </a>
+  )
+}
+
+export function Bar() {
+  return (
+    <header className="flex font-medium text-2xl flex-row w-screen items-center justify-around px-4 py-1 h-min mt-5 ">
+      <div>
+        <h1 className="text-5xl">&lt;Romy /&gt;</h1>
+      </div>
+      <div className="flex flex-row space-x-10 text-slate-600">
+        <LinkToSection section="about">About Me</LinkToSection>
+        <LinkToSection section="Projects">Projects</LinkToSection>
+        <LinkToSection section="Contact">Contact</LinkToSection>
+      </div>
+    </header>
+  )
+}
