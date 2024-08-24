80901a93b5b497b0ecc429be6cc0f8bf96fe388f
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-23 22:44:32 -0300

    Data

---
+++ src/data.ts
@@ -0,0 +1,30 @@
+export const PROJECTS = [
+  {
+    title: 'FoodOrder',
+    technologies: ['React', 'css'],
+    img: 'foodOrder.png',
+    link: 'https://food-order-blue.vercel.app/',
+    repositorio: 'https://github.com/RomyMartinez/foodOrder',
+  },
+  {
+    title: 'FoodOrder',
+    technologies: ['react', 'css'],
+    img: 'images/food',
+    link: 'https://food-order-5gmb2cmsf-romy-martinezs-projects.vercel.app/',
+    repositorio: 'https://github.com/RomyMartinez/foodOrder',
+  },
+  {
+    title: 'FoodOrder',
+    technologies: ['react', 'css'],
+    img: 'images/food',
+    link: 'https://food-order-5gmb2cmsf-romy-martinezs-projects.vercel.app/',
+    repositorio: 'https://github.com/RomyMartinez/foodOrder',
+  },
+  {
+    title: 'FoodOrder',
+    technologies: ['react', 'css'],
+    img: 'images/food',
+    link: 'https://food-order-5gmb2cmsf-romy-martinezs-projects.vercel.app/',
+    repositorio: 'https://github.com/RomyMartinez/foodOrder',
+  },
+]
