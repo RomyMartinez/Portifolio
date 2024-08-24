15dc69d5820129198ce2e5b6fa5d12fe1aa0456e
Romy Martinez <ngcsmrtnz@gmail.com>
2024-08-23 23:09:30 -0300

    Projects delete import React.svg

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,7 @@
 import { PROJECTS } from '../data'
 import { Button } from './UI/Button'
 import { Title } from './UI/Title'
-import React from 'assets/React.svg'
+import { Icon } from './UI/Icon'
 
 export function Projects() {
   return (
@@ -11,40 +11,34 @@ export function Projects() {
         {PROJECTS.map((project) => {
           return (
             <li key={project.title}>
-              <div className="flex flex-col bg-slate-700 rounded-lg h-80 hover:scale-105 transition duration-600 ease-in-out p-2">
+              <div className="flex flex-col bg-slate-100 border-2 border-slate-800 rounded-lg h-80 hover:scale-105 transition duration-600 ease-in-out p-2">
                 <img
                   className="rounded-lg h-52"
                   src={`../public/projetos/${project.img}`}
                   alt={project.title}
                 />
-                <h3 className="text-xl text-slate-200 font-bold">
+                <h3 className="text-xl text-slate-800 font-bold p-2">
                   {project.title}
                 </h3>
                 <div className="flex justify-between">
                   <div className="flex gap-5">
-                    {project.technologies.map((technique) => {
-                      return (
-                        <img
-                          className="h-6 w-6"
-                          src={`${technique}`}
-                          alt={technique}
-                        />
-                      )
-                    })}
+                    <ul className="flex  items-center">
+                      {project.technologies.map((technique) => {
+                        return (
+                          <li key={technique}>
+                            <Icon name={technique} />
+                          </li>
+                        )
+                      })}
+                    </ul>
                   </div>
                   <div className="flex gap-2">
-                    <Button
-                      extraClass="hover:border-2 hover:border-slate-300 border-slate-700 "
-                      textOnly
-                    >
+                    <Button extraClass="hover:border-2 hover:border-slate-300 hover:border-2 hover:border-slate-700 border-slate-700 ">
                       <a target="_blank" href={project.link}>
                         Visit Project
                       </a>
                     </Button>
-                    <Button
-                      extraClass="hover:border-2 hover:border-slate-300 border-slate-700"
-                      textOnly
-                    >
+                    <Button extraClass="hover:border-2 hover:border-slate-300 hover:border-2 hover:border-slate-700 border-slate-700">
                       <a target="_blank" href={project.repositorio}>
                         Repositorio
                       </a>
