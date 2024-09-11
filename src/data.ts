2469a6b1d602461e6604aa8e7ad887a5fe5a4ff4
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2024-09-11 16:47:29 -0300

    Vercel image

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,7 @@ export const PROJECTS = [
   {
     title: 'FoodOrder',
     technologies: ['css', 'React'],
-    img: 'foodOrder.png',
+    img: 'projetos/foodOrder.png',
     link: 'https://food-order-blue.vercel.app/',
     repositorio: 'https://github.com/RomyMartinez/foodOrder',
   },
